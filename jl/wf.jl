36d4192709dbe63679d196caa9ef7d88f49179de
Patrizia M. Favaron <mafavaron@mac.com>
2020-10-10 06:01:24 +0200

    Update wf.jl
    In progress

--- jl/wf.jl
+++ jl/wf.jl
@@ -6,6 +6,9 @@ module WildFlower
     end
 
     function Initialize(p::Point)
+        local theta = 2.*pi*rand(Float64)
+        local rho   = rand(Float64)
+        point.x = rho*cos(alpha)
     end
 
 end
